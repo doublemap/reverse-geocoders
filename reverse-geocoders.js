3a33e3e008727aff625c788a3651d299619cd54d
Eric Jiang <eric@doublemap.com>
2014-05-15 11:54:58 -0400

    Add apache 2.0 license notice to code

--- reverse-geocoders.js
+++ reverse-geocoders.js
@@ -1,3 +1,19 @@
+/*
+Copyright 2014 DoubleMap Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 var http = require('http');
 var sprintf = require('sprintf').sprintf;
 var request = require('request');
@@ -8,7 +24,8 @@ var Q = require('q');
  * Given a lat, lon, returns:
    a Q promise that could give:
    {
-      "address": "238 Deacon St",
+      "street_number": "238",
+      "street": "Deacon St",
       "city": "Indianapolis",
       "state": "IN",
       "postal_code": "46202",
